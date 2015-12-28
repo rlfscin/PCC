72732346c0a21ac978b15fb2991c6835adc6ba25
pedrootorres <phtg@cin.ufpe.br>
2015-12-28 15:47:24 -0300

    Structure ready, missing algorithms

--- PCC2/ipmt.cpp
+++ PCC2/ipmt.cpp
@@ -3,6 +3,8 @@
 #include <getopt.h>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
 
 using namespace std;
@@ -13,18 +15,20 @@ int main(int argc, char* argv[]) {
 	char *indextype = NULL;
 	char *compression = NULL;
 	char *patterns = NULL;
+	bool count = false;
 
 	while(1) {
 		static struct option long_options[] = {
-			{"indextype", required_argument, 0, 1},
-			{"compression", required_argument, 0, 2},
-			{"pattern", required_argument, 0, 'p'},
-			{"help", no_argument, 0, 'h'}
+			{"indextype", 	required_argument, 0,  1 },
+			{"compression", required_argument, 0,  2 },
+			{"pattern", 	required_argument, 0, 'p'},
+			{"count", 		no_argument,	   0, 'c'},
+			{"help", 		no_argument, 	   0, 'h'}
 		};
 
 		int option_index = 0;
 
-		c = getopt_long (argc, argv, "p:a", long_options, &option_index);
+		c = getopt_long (argc, argv, "", long_options, &option_index);
 
 		if(c == -1)
 			break;
@@ -42,15 +46,57 @@ int main(int argc, char* argv[]) {
 				patterns = optarg;
 				break;
 
+			case 'c':
+				count = true;
+				break;
+
 			case 'h':
 				cout << "ipmt [index] [--indextype=suffixtree] [--indextype==suffixarray] [--compression=lz77] [--compression=lz78] [search] [-p --pattern patternfile] [pattern] file" << endl;
 				return 0;
 
 			default:
-				printf("no option");
+				printf("no option\n");
 				return 0;
 		}
 	}
 
-	
+	printf("indextype=%s, compression=%s, option=%s\n", indextype, compression, argv[optind]);
+
+	if(strcmp(argv[optind], "index") == 0) {
+		printf("Here we do the index\n");
+
+		/*
+		* Checar qual index no indextype, default = ?
+		* Checar qual compressao no comprresion, default = lz77
+		* Pegar arquivo de entrada
+		* Fazer a indexacao e depois compressao. guardar arquivo
+		*/
+
+	} else if(strcmp(argv[optind], "search") == 0){
+		printf("Here we do the search\n");
+
+		vector<string> theseAreThePatterns;
+
+		if(patterns == NULL) {
+			// only one pattern
+			theseAreThePatterns.push_back(argv[++optind]);
+		} else {
+			// file with patterns
+			ifstream patternsFile(patterns);
+			string line;
+
+			while(getline(patternsFile, line)) {
+				theseAreThePatterns.push_back(line);
+			}
+		}
+
+		/*
+		* Pegar arquivo de entrada
+		* Descomprimir e fazer busca
+		* Imprimir ocorrencias
+		*/
+
+	} else {
+		printf("%s is not an option\n", argv[optind]);
+	}
 }
