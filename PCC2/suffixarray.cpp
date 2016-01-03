04dae94440b5780b9895a0476f20ebdb16e743f9
pedrootorres <phtg@cin.ufpe.br>
2016-01-03 19:23:26 -0300

    Suffix array working for big strings. stil needs to write to a file

--- PCC2/suffixarray.cpp
+++ PCC2/suffixarray.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iterator>
 
 using namespace std;
 
@@ -30,24 +31,24 @@ public:
 			P[i] = text[i];
 		}
 
-		int count = 1;
+		// int count = 1;
 		int lg2 = ceil(log2(n));
-		x L[n];
-		cout << "morreu na linha acima" <<endl;
+		vector<x> L(n);
 
 
 		for(int k = 1; k < lg2; k++) {
 			for (int i = 0; i < n; i++) {
 				L[i].a = P[i];
-				L[i].b = i + count < n ? P[i + count] : -1;
+				L[i].b = i + 1 < n ? P[i + 1] : -1;
 				L[i].index = i;
 			}
 
-			sort(L, L + n, this->cmp);
+			sort(L.begin(), L.end(), this->cmp);
 
 			for (int i = 0; i < n; i++) {
 				P[L[i].index] = i > 0 && L[i].a == L[i-1].b && L[i].b == L[i-1].b ? P[L[i-1].index] : i;
 			}
+			// count *= 2;
 		}
 
 		return P;
@@ -60,7 +61,7 @@ int main() {
 	ifstream in;
 	std::ostringstream contents;
 	string contentsStr;
-	in.open("textoparaindice.txt");
+	in.open("dna.50MB");
 	contents.str("");
 	contents << in.rdbuf();
 	in.close();
@@ -68,12 +69,27 @@ int main() {
 
 	vector<int> a = teste.index(contentsStr);
 
-	for (int i = 0; i < a.size(); ++i)
-	{
-		cout << a[i] << " ";
-	}
+	// ofstream outfile ("indices.idx", ios::binary);
+	// outfile.write((char *) &a, sizeof(a));
+	// outfile.close();
 
-	cout << "\n";
+	// ofstream FILE("indices.idx", ios::out | ofstream::binary);
+	// copy(a.begin(), a.end(), ostreambuf_iterator<char>(FILE));
+
+	// vector<int> newVector;
+	// ifstream INFILE("indices.idx", ios::out | ofstream::binary);
+	// istreambuf_iterator<char> iter(INFILE);
+	// copy(iter.begin(), iter.end(), back_inserter(newVector));
+
+	// vector<int> toRestore;
+	// ifstream infile("indices.idx", ios::binary);
+	// infile.read((char *) &toRestore, sizeof(toRestore));
+	// infile.close();
+
+	// for (int i = 0; i < a.size(); i++)
+	// {
+	// 	cout << a[i] << endl;
+	// }
 
 	return 0;
 }
