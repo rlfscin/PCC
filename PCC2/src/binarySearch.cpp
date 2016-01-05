73c5ef74a781df4658c613c71ca69cc2618164f4
Bruno Assis <bap2@cin.ufpe.br>
2016-01-05 17:12:13 -0300

    correcoes e revisoes sobre o binarySearch

--- PCC2/src/binarySearch.cpp
+++ PCC2/src/binarySearch.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <string>
+
 using namespace std;
 
 class binarySearch {
@@ -6,22 +9,34 @@ public:
 
 	}
 
+	string getSuffix(string S, int size) {
+		if (S.length() <= size) {
+			return S;
+		} else {
+			return S.substr(0, size);
+		}
+	}
+	
 	int findSucessor(string T, string P, vector<int> S) {
 		// P <=m T[S1..]
-		if (P <= T.substr(S[0]).substr(P.length())) {
+		if (P <= getSuffix(T.substr(S[0]), P.length())) {
 			return 0;
+			
+		}
 		// P >=m T[Sn..]
-		} else if (P >= T.substr(S[S.size()-1]).substr(P.length())) {
+		else if (P >= getSuffix(T.substr(S[S.size() - 1]), P.length())) {
 			return S.size();
-		} else {
-			int l = 0, r = S.size()-1;
+		}
+		else {
+			int l = 0, r = S.size() - 1;
 			int h = 0;
 			while (r - l > 1) {
-				h = floor((l+r)/2);
+				h = floor((l + r) / 2);
 				// P <=m T[Sh..]
-				if (P <= T.substr(S[h]).substr(P.length())) {
+				if (P <= getSuffix(T.substr(S[h]), P.length())) {
 					r = h;
-				} else {
+				}
+				else {
 					l = h;
 				}
 			}
@@ -31,20 +46,24 @@ public:
 
 	int findPredecessor(string T, string P, vector<int> S) {
 		// T[Sn..] <=m P
-		if (T.substr(S[S.size()-1].substr(P.length())) <= P) {
+		if (getSuffix(T.substr(S[S.size() - 1]), P.length()) <= P) {
 			return S.size();
+			
+		}
 		// T[S1..] <=m P
-		} else if (T.substr(S[0]).substr(P.length()) <= P) {
+		else if (getSuffix(T.substr(S[0]), P.length()) >= P) {
 			return 0;
-		} else {
-			int l = 0, r = S.size()-1;
+		}
+		else {
+			int l = 0, r = S.size() - 1;
 			int h = 0;
-			while(r - l > 1) {
-				h = floor((l+r)/2);
+			while (r - l > 1) {
+				h = floor((l + r) / 2);
 				// T[Sh..] <=m P
-				if (T.substr(S[h]).substr(P.length()) <= P) {
+				if (getSuffix(T.substr(S[h]), P.length()) <= P) {
 					l = h;
-				} else {
+				}
+				else {
 					r = h;
 				}
 			}
@@ -52,5 +71,24 @@ public:
 		}
 	}
 
-	
+	vector<string> search(string T, string P, vector<int> S) {
+		int predecessor = findPredecessor(T, P, S);
+		int sucessor = findSucessor(T, P, S);
+		printf("predecessor: %d\nsucessor: %d\n", predecessor, sucessor);
+		vector<string> matches(predecessor - sucessor + 1);
+		int j = 0;
+		for (int i = sucessor; i <= predecessor; i++)
+		{
+			matches[j] = T.substr(S[i]);
+			j++;			
+		}
+		printf("yam");
+		return matches;
+	}
+};
+
+int main() {
+	binarySearch bs;
+
+
 }
